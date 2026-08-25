150aea165edc251047e63442594f66b0db25a38b
fennerii <jonathanfennerii@gmail.com>
2026-08-25 13:29:05 -0400

    name

---
+++ moocjp1/reading_input/name/name.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class name{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What's your name?");
+        String name = scanner.nextLine();
+        System.out.println("Hi " + name );
+
+    }
+}
