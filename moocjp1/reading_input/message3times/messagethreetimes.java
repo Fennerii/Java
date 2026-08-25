ffd8c43419d7586d2914bb108b0caa731b93dbae
fennerii <jonathanfennerii@gmail.com>
2026-08-25 13:24:53 -0400

    message 3 times

---
+++ moocjp1/reading_input/message3times/messagethreetimes.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class messagethreetimes{
+    public static void main(String[] args) {
+        Scanner scanner =  new Scanner(System.in);
+
+        System.out.println("Write a message 3 times");
+        String message = scanner.nextLine();
+
+        System.out.println(message);
+        System.out.println(message);
+        System.out.println(message);
+        
+    }
+}
