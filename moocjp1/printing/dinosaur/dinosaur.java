959de12848b5d44f2b30a707ae860660071e1a2f
fennerii <jonathanfennerii@gmail.com>
2026-08-25 13:53:45 -0400

    format

--- moocjp1/printing/dinosaur/dinosaur.java
+++ moocjp1/printing/dinosaur/dinosaur.java
@@ -1,4 +1,4 @@
-public class dinosaur{
+public class dinosaur {
     public static void main(String[] args) {
         System.out.println("Once upon a time");
         System.out.println("there was");
