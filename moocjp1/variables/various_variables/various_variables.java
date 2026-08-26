351532b15bdc7cd84d44a9bf2f1a2106e895ed8d
fennerii <jonathanfennerii@gmail.com>
2026-08-26 17:56:20 -0400

    variables

---
+++ moocjp1/variables/various_variables/various_variables.java
@@ -0,0 +1,15 @@
+public class various_variables{
+    public static void main(String[] args){
+
+        int chickenQuantity = 3;
+        double bacon = 5.5;
+        String tractor = "none!";
+
+
+        System.out.println("Chicken \n" + chickenQuantity);
+        System.out.println("Bacon (kg): \n" + 5.5);
+        System.out.println("Tractor:\n" + tractor);
+
+        System.out.println("And finally, a summary:\n" + chickenQuantity + "\n" + bacon + "\n"+ tractor);
+    }
+}
