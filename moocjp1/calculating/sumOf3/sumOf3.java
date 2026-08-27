64f55ae719155510950fc92022f1965ab0446e76
fennerii <jonathanfennerii@gmail.com>
2026-08-27 13:54:49 -0400

    sum of 3

---
+++ moocjp1/calculating/sumOf3/sumOf3.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+public class sumOf3{
+ 
+public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+
+    System.out.print("Give the first number:");
+    int FirstNum = Integer.valueOf(s.nextLine());
+    System.out.println("Give the second number:");
+    int SecNum = Integer.valueOf(s.nextLine());
+    System.out.println("Give the third number:");
+    int ThirdNum = Integer.valueOf(s.nextLine());
+
+    int total = FirstNum + SecNum + ThirdNum;
+
+    System.out.println("The sum of the numbers is "+ total);
+
+
+
+}
+
+        
+    
+}
